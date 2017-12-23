cffdfadb549e02e6b4f80ec1bfc2cdd0598993b0
jpb <jpb.06@outlook.com>
2017-12-23 13:19:39 +0100

    Online players fetching as a self contained task: uses a 2 minutes threshold to prevent spaming the server

---
+++ business/tasks/fetch.online.players.task.js
@@ -0,0 +1,31 @@
+const moment = require('moment');
+
+const discoAuthRequest = require('./../requests/discogc.authentication.js');
+const discoOnlinePlayersRequest = require('./../requests/discogc.online.players.js');
+
+const onlinePlayersDal = require('./../../dal/mongodb/dal.online.players.js');
+const lastFetchDal = require('./../../dal/mongodb/dal.last.fetch.js');
+
+let unit = module.exports = {
+    "start": async () => {
+        let lastFetch = await lastFetchDal.get();
+        let now = moment();
+
+        // request threshold to discogc = 2 minutes 
+        if (!lastFetch || moment(lastFetch.date).add(2, 'm').isBefore(now)) {
+            let cookies = await discoAuthRequest.send();
+            let data = await discoOnlinePlayersRequest.send(cookies);
+
+            // ??? why escaped quotes
+            let online = JSON.parse(data.replace(/\\"/g, '"'));
+
+            await onlinePlayersDal.set(online.Players);
+            await lastFetchDal.set(moment().format());
+
+            return online.Players;
+        } else {
+            let onlinePlayers = await onlinePlayersDal.get();
+            return onlinePlayers;
+        }
+    }
+}
