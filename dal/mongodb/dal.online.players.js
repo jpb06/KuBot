2b987234472ee9b047e6f2f394a8b99ffaa3877d
jpb <jpb.06@outlook.com>
2017-12-23 13:16:15 +0100

    Updating dal

---
+++ dal/mongodb/dal.online.players.js
@@ -0,0 +1,43 @@
+const MongoClient = require("mongodb").MongoClient;
+
+let unit = module.exports = {
+    "set": async (onlinePlayers) => {
+        const client = await MongoClient.connect(process.env.mongodbUrl);
+        let db = client.db(process.env.mongodbBase);
+
+        try {
+            await db.dropCollection('onlineplayers');
+            let collection = db.collection('onlineplayers');
+
+            await collection.insertMany(onlinePlayers, null);
+        } finally {
+            client.close();
+        }
+    },
+    "get": async () => {
+        const client = await MongoClient.connect(process.env.mongodbUrl);
+        let db = client.db(process.env.mongodbBase);
+
+        try {
+            let collection = db.collection('onlineplayers');
+            const result = await collection.find().toArray();
+
+            return result;
+        } finally {
+            client.close();
+        }
+    },
+    "getByRegion": async (region) => {
+        const client = await MongoClient.connect(process.env.mongodbUrl);
+        let db = client.db(process.env.mongodbBase);
+
+        try {
+            let collection = db.collection('onlineplayers');
+            const result = await collection.find({ Region: region }).toArray();
+
+            return result;
+        } finally {
+            client.close();
+        }
+    }
+}
