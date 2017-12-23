b19fe6e717205ec0af9a23681d4f1a4bf56a70f4
jpb <jpb.06@outlook.com>
2017-12-23 13:29:40 +0100

    frontend response related (embed)

---
+++ business/commands/shared/commands.description.js
@@ -0,0 +1,10 @@
+let unit = module.exports = {
+    "scanUsage": () => {
+        return 'Command usage :\n' +
+            '```!scan```\n';
+    },
+    "helpUsage": () => {
+        return 'Command usage :\n' +
+            '```!help```\n';
+    }
+}
