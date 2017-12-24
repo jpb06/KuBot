270b4d0a8837d9e81e995cc289bc491561951eda
jpb <jpb.06@outlook.com>
2017-12-24 09:33:52 +0100

    Updating Kusari initializer

--- dal/initializers/dal.kusari.initializer.js
+++ dal/initializers/dal.kusari.initializer.js
@@ -1,4 +1,5 @@
 const dalFactions = require("./../mongodb/dal.factions.watch.js");
+const dalRegions = require("./../mongodb/dal.regions.watch.js");
 
 let unit = module.exports = {
     "initialize": async () => {
@@ -8,5 +9,16 @@ let unit = module.exports = {
         dalFactions.add('kishiro', 'Kishiro', ['Kishiro|'], false);
         dalFactions.add('samura', 'Samura', ['Samura|-'], false);
         dalFactions.add('hogosha', 'Hogosha', ['[HA]'], false);
+
+        dalRegions.add('kusari', 'Kusari', [
+            'Nagano', 'Chugoku', 'Tohoku', 'Rishiri',
+            'Tottori', 'Hokkaido', 'Hiroshima',
+            'New Tokyo', 'Honshu', 'Kyushu',
+            'Shikoku'], true);
+        dalRegions.add('libertyborder', 'Liberty border', ['Kepler', 'Galileo'], false);
+        dalRegions.add('taus', 'Taus', ['Tau-29', 'Tau-37', 'Tau-23', 'Tau-44'], false);
+        dalRegions.add('sigmas', 'Sigmas', ['Sigma-19', 'Sigma-13', 'Sigma-21', 'Sigma-17', 'Okinawa'], false);
+
+
     }
 }
