dbb08d738c755d5fd5f76628729db5d15f9041da
jpb <jpb.06@outlook.com>
2017-12-24 12:53:10 +0100

    Persistence operations for players watch

---
+++ dal/mongodb/dal.players.watch.js
@@ -0,0 +1,35 @@
+const MongoClient = require("mongodb").MongoClient;
+
+let unit = module.exports = {
+    "get": async () => {
+        const client = await MongoClient.connect(process.env.mongodbUrl);
+        let db = client.db(process.env.mongodbBase);
+
+        try {
+            let collection = db.collection('playerswatch');
+
+            const result = await collection.find().toArray();
+
+            return result;
+        } finally {
+            client.close();
+        }
+    },
+    "add": async (name, comment) => {
+        const client = await MongoClient.connect(process.env.mongodbUrl);
+        let db = client.db(process.env.mongodbBase);
+
+        try {
+            let collection = db.collection('playerswatch');
+
+            await collection.findOneAndUpdate(
+                { name: name },
+                { name: name, comment: comment },
+                { upsert: true }
+            );
+
+        } finally {
+            client.close();
+        }
+    }
+}
