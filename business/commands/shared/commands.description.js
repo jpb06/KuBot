f5d164993d8307481ddccd9bcea9476fb8e05160
jpb <jpb.06@outlook.com>
2017-12-25 12:13:43 +0100

    Adding watch to commands description

--- business/commands/shared/commands.description.js
+++ business/commands/shared/commands.description.js
@@ -6,5 +6,15 @@ let unit = module.exports = {
     "helpUsage": () => {
         return 'Command usage :\n' +
             '```!help```\n';
+    },
+    "watchUsage": () => {
+        return 'Command usage :\n' +
+            '```!watch name \'comment\'```\n' +
+            'Where :\n' +
+            '\t**__name__** is the player ingame name.\n' +
+            '\t**__comment__** is a comment related to this player (optional).\n\n' +
+            'Example :\n```!watch Innocent.Bystander \'Cardamine smuggler\'\n' +
+            '!watch Hayagfgdf.Kimiko \"Mean person\"\n' +
+            '!watch Ishikawa.Hideaki```\n';
     }
 }
