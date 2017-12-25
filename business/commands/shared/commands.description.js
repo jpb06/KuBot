2ed3a53214c660836e5a6875ad8c9c5f56995a4d
jpb <jpb.06@outlook.com>
2017-12-25 17:37:25 +0100

    Adding show command description

--- business/commands/shared/commands.description.js
+++ business/commands/shared/commands.description.js
@@ -16,5 +16,14 @@ let unit = module.exports = {
             'Example :\n```!watch Innocent.Bystander \'Cardamine smuggler\'\n' +
             '!watch Hayagfgdf.Kimiko \"Mean person\"\n' +
             '!watch Ishikawa.Hideaki```\n';
+    },
+    "showUsage": () => {
+        return 'Command usage :\n' +
+            '```!show term```\n' +
+            'Where :\n' +
+            '\t**__term__** is :\n' +
+            '\t\tplayers (shorthand p), to display watched players list\n\n' +
+            'Example :\n```!show players\n' +
+            '!show p```\n';
     }
 }
