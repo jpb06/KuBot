97d80db85b1123e6cefb79b341d1dac3ce37b178
jpb <jpb.06@outlook.com>
2017-12-26 13:59:53 +0100

    Updating show command to display factions watch

--- business/commands/show.command.js
+++ business/commands/show.command.js
@@ -1,7 +1,9 @@
 const argumentsValidation = require('./shared/arguments.validation.js');
 const embedHelper = require('./../util/embed.helper.js');
-const playersWatchDal = require('./../../dal/mongodb/dal.players.watch.js');
 const commandsDescriptions = require('./shared/commands.description.js');
+
+const playersWatchDal = require('./../../dal/mongodb/dal.players.watch.js');
+const factionsWatchDal = require('./../../dal/mongodb/dal.factions.watch.js');
 
 let unit = module.exports = {
     "process": async (args, message, client) => {
@@ -17,11 +19,42 @@ let unit = module.exports = {
                 )
             });
         } else {
-            let watchedPlayers = await playersWatchDal.get();
+            if (args === 'players' || args === 'p') {
+                let watchedPlayers = await playersWatchDal.get();
 
-            message.channel.send({
-                embed: embedHelper.watchedPlayers(watchedPlayers)
-            });
+                let description = '';
+                watchedPlayers
+                    .sort((a, b) => a.name < b.name)
+                    .forEach(player => {
+                        description += '- **' + player.name + '**';
+                        if (player.comment) description += ' - ' + player.comment;
+                        description += '\n';
+                    });
+
+                message.channel.send({
+                    embed: embedHelper.show(watchedPlayers.length, description, 'Players')
+                });
+            } else if (args === 'factions' || args === 'f') {
+                let watchedFactions = await factionsWatchDal.get();
+
+                let description = '';
+                watchedFactions
+                    .sort((a, b) => a.name < b.name)
+                    .forEach(faction => {
+                        description += `- **${faction.name}** (${faction.identifier})\n`;
+                        if (faction.alwaysDisplay) description += '\tAlways displayed\n';
+                        description += '\tTags: ';
+
+                        faction.tags.forEach(tag => {
+                            description += `**${tag}**, `;
+                        });
+                        description = description.slice(0, -2) + '\n\n';
+                    });
+
+                message.channel.send({
+                    embed: embedHelper.show(watchedFactions.length, description, 'Factions')
+                });
+            }
         }
     }
 }
