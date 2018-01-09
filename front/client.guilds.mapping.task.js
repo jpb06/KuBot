1cc0046e771d4d8e74da03033bde19f4e4085747
jpb <jpb.06@outlook.com>
2018-01-09 18:23:59 +0100

    Removing test code leftover

--- front/client.guilds.mapping.task.js
+++ front/client.guilds.mapping.task.js
@@ -14,25 +14,6 @@ let unit = module.exports = {
                     defaultChannel: guild.channels.find(channel => channel.name === guildSettings.mainChannel),
                     emergencyChannel: guild.channels.find(channel => channel.name === guildSettings.emergencyChannel)
                 });
-
-                if (guild.id === '350303355014479874') {
-
-                    let gm = guildsMapping.find(g => g.id === guild.id);
-
-                    let mess = await gm.emergencyChannel.fetchMessages({ limit: 1 });
-
-                    // kubot id = 385798675533660160
-                    let mes = mess.first();
-                    if (mes.author.id === '385798675533660160') {
-                        await mes.edit(new Discord.RichEmbed()
-                            .setThumbnail('https://i.imgur.com/5L7T68j.png')
-                            .setTimestamp(new Date())
-                            .setFooter('kuBot', 'https://i.imgur.com/5L7T68j.png')
-                            .setColor(10684167)
-                            .setTitle('test2'));
-                    }
-                }
-
             });
 
             return guildsMapping;
