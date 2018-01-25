23b6c1cf16cd63945a99bca4fedaa6c46dfcbbb3
jpb <jpb.06@outlook.com>
2018-01-25 08:33:58 +0100

    Fixing errors logs format (new line)

--- business/util/errors.logging.helper.js
+++ business/util/errors.logging.helper.js
@@ -4,7 +4,7 @@ const filesHelper = require('./files.helper.js');
 
 let unit = module.exports = {
     "save": async (error) => {
-        let desc = `\n-----------------------------\n${moment().format('MM/DD/YYYY HH:mm:ss')}\n${error.stack}`;
+        let desc = `\r\n-----------------------------\r\n${moment().format('MM/DD/YYYY HH:mm:ss')}\r\n${error.stack}`;
         await filesHelper.append('./err.log', desc);
         return true;
     }
