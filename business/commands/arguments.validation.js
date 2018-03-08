b026f2bdee4f43d9bd12a6d8a52eabdd94572ba1
jpb <jpb.06@outlook.com>
2018-03-08 23:32:52 +0100

    Arguments validation for quote, quotetext and embed commands

--- business/commands/arguments.validation.js
+++ business/commands/arguments.validation.js
@@ -44,5 +44,29 @@ let unit = module.exports = {
             return 'Illegal first parameter\n';
 
         return '';
+    },
+    "checkQuoteMessageArgs": (args) => {
+        if (args.length !== 1)
+            return 'Expecting one argument\n';
+        if (!/^\+?(0|[1-9]\d*)$/.test(args[0]))
+            return 'Argument should be a number\n';
+
+        return '';
+    },
+    "checkQuoteTextArgs": (args) => {
+        if (args.length === 0)
+            return 'Expecting text after the command\n';
+
+        return '';
+    },
+    "checkEmbedArgs": (args) => {
+        if (args.length !== 2)
+            return 'Expecting a title and a message\n';
+        if (args[0].length === 0)
+            return 'The embed title cannot be empty\n';
+        if (args[1].length === 0)
+            return 'The embed content cannot be empty\n';
+
+        return '';
     }
 }
